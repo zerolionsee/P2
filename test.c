126e921621c4628c980414159e9ad223b7c445b4
zerolionsee <hekun6688@163.com>
2022-05-06 16:45:43 +0800

    20220506

--- test.c
+++ test.c
@@ -138,3 +138,49 @@ int main()
   int max=MAX(a,b);
   printf("max=%d\n",max);
 } */
+/* #include <stdio.h>
+int main()
+{
+  int a = 1;
+  int b = 2;
+  int c = 2;
+  int d = 3;
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int i = 0;
+  while(i<10)
+  {
+    printf("%d\n", arr[i]);
+    i++;
+  }
+} */
+/* #include <stdio.h>
+int main()
+{
+  int a = 3;
+  int b = 4;
+  int c;
+  c = a & b;
+  printf("%d\n", c);
+} */
+/* #include <stdio.h>
+int main()
+{
+  int a = 10;
+  printf("%d\n", a);
+  printf("%d\n", !a);
+} */
+#include <stdio.h>
+int main()
+{
+  // int a = 10;
+  // printf("%d\n", sizeof(a));
+  // printf("%d\n", sizeof(int));
+  // printf("%d\n", sizeof a);
+  // printf("%d\n", sizeof int);
+  int arr[10]={0};
+  int n=0;
+  printf("%d\n", sizeof arr);
+  printf("%d\n", sizeof(arr));
+  printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+  return 0;
+}
